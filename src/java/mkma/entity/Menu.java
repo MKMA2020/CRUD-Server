a1c5d30bb3a0b365891282743e26edc379fe51bc
Martin Valiente Ainz <2dam@MACINF01.TartangaLH.eus>
2020-12-01 10:00:43 +0100

    Worked on Menu JPA.

--- src/java/mkma/entity/Menu.java
+++ src/java/mkma/entity/Menu.java
@@ -1,22 +1,40 @@
 package mkma.entity;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 /**
- *Claas for the menu and I'ts methods
- * @author 2dam
+ * The Menu class will contain the information of one Menu.
+ * @author Kerman Rodriguez
  */
 @Entity
 @Table (name="menu", schema="mkma")
 public class Menu implements Serializable{
     private static final long serialVersionUID = 1L;
+    
+    /**
+     * id Will contain the ID of the Ingredient.
+     */
     @Id
     @GeneratedValue( strategy = GenerationType.AUTO )
     private Long id;
+    
+    /**
+     * name Will contain the name of the Menu.
+     */
     @NotNull
     private String name;
+    
+    /**
+     * type Will contain the type of the Menu.
+     */
     @NotNull
-    private menuType menuType;
+    private menuType type;
     
    
     public Long getId() {
@@ -35,27 +53,49 @@ public class Menu implements Serializable{
         this.name = name;
     }
 
-    public menuType getMenuType() {
-        return menuType;
+    public menuType getType() {
+        return type;
     }
 
-    public void setMenuType(menuType menuType) {
-        this.menuType = menuType;
+    public void setType(menuType type) {
+        this.type = type;
     }
     
-    
-    
-    
-    
-    
-/**
- * enum declarated outside the main body.
- */   
+      @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Menu)) {
+            return false;
+        }
+        Menu other = (Menu) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mkma.entity.Menu[ id=" + id + " ]";
+    }
+
 }
+
+/**
+ * This enum will have the different types of Menus.
+ *
+ * @author Kerman Rodriguez
+ */
 enum menuType {
     Breakfast,
     Lunch,
     Snack,
     Dinner
 }
-
