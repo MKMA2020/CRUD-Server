b5054d6e4046feb65e1019ec2050ebab9cbf490a
2dam <2dam@MACINF09.TartangaLH.eus>
2020-12-01 11:46:08 +0100

    Handdle window showing has been fixed in order to the feautures to work when launching the windows.

--- src/java/mkma/entity/Recipe.java
+++ src/java/mkma/entity/Recipe.java
@@ -1,35 +1,52 @@
 package mkma.entity;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
 /**
  * Class will contain the information of a recipe.
  *
  * @author Martin Gros
  */
-public class Recipe {
+@Entity
+
+public class Recipe implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     /**
      * id Will contain the ID of the Recipe.
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     /**
      * Name Will contain the Name of the Recipe.
      */
+    @NotNull
     private String Name;
 
     /**
      * steps Will contain the steps of the Recipe.
      */
+    @NotNull
     private String steps;
 
     /**
      * kCal Will contain the kCal value of the Recipe.
      */
+    @NotNull
     private float kCal;
 
     /**
      * type Will contain the type of the Recipe.
      */
+    @NotNull
     private RecipeType type;
 
     public Long getId() {
@@ -72,6 +89,30 @@ public class Recipe {
         this.type = type;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Recipe)) {
+            return false;
+        }
+        Recipe other = (Recipe) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mkma.entity.Recipe[ id=" + id + " ]";
+    }
 }
 
 /**
