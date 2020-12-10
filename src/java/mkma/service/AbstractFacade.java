1aa79118f12f89873b4327d0ff828da4093c6f8e
2dam <2dam@MACINF09.TartangaLH.eus>
2020-12-10 12:50:26 +0100

    Queries made, abstract facade and recipefacade implemented. Code commented.

--- src/java/mkma/service/AbstractFacade.java
+++ src/java/mkma/service/AbstractFacade.java
@@ -1,6 +1,9 @@
 package mkma.service;
 
+import java.util.List;
 import javax.persistence.EntityManager;
+import mkma.entity.Recipe;
+import mkma.enumeration.RecipeType;
 
 /**
  * 
@@ -32,4 +35,34 @@ public abstract class AbstractFacade<T> {
     public T find(Object id) {
         return getEntityManager().find(entityClass, id);
     }
+    /**
+     * finds all recipes and orders them by their name
+     * @return a list of recipes
+     */
+    public List <Recipe> findAllRecipes (){
+        List <Recipe> recipes;
+        recipes = getEntityManager().createNamedQuery("findAllRecipes").getResultList();
+        return recipes;
+    }
+    /**
+     * Receives the type and orders all receipes by their type
+     * @param type
+     * @return a list of recipes
+     */
+
+    public List<Recipe> getRecipesByType(RecipeType type) {
+        List <Recipe> recipes;
+        recipes = getEntityManager().createNamedQuery("getRecipesByType").getResultList();
+        return recipes;
+    }
+    /**
+     * Orders all receipes by their calorie count
+     * @return a list of recipes
+     */
+    public List<Recipe> OrderBykCal() {
+        List <Recipe> recipes;
+        recipes = getEntityManager().createNamedQuery("OrderBykCal").getResultList();
+        return recipes;
+    }
 }
+
